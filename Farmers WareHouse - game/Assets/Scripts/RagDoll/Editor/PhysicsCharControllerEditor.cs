504378f1d3372d1de353b705e5a69d71599f65c6
Eurico Martins <euricomartinss@gmail.com>
2020-08-05 21:53:32 +0100

    Refactoring approach

--- Farmers WareHouse - game/Assets/Scripts/RagDoll/Editor/PhysicsCharControllerEditor.cs
+++ Farmers WareHouse - game/Assets/Scripts/RagDoll/Editor/PhysicsCharControllerEditor.cs
@@ -1,90 +1,15 @@
 ﻿using UnityEditor;
+using UnityEditor.Experimental.TerrainAPI;
 using UnityEngine;
 
+// Custom editor para a class de comportamento fisico
 [CustomEditor(typeof(PhysicsCharacterController))]
 public class PhysicsCharControllerEditor : Editor
 {
-    // Vista de editor personalizada para o controlo fisico do personagem
-    // referencia para o objecto de raiz do esqueleto
-    // referencia para oarray de ossos uteis
-    private SerializedProperty skeletonBase;
-    private PhysicsCharacterController physicsCtrl_target;
-
+    // override para o default inspector
     public override void OnInspectorGUI()
     {
-        // Referencia para o objecto de raiz do controlador
-        GUIReferences();
-    }
-
-    #region Private Methods
-    // comporta todos a atualizaçao das referencia para objectos
-    private void GUIReferences()
-    {
-        // guarda a referencia para o script
-        physicsCtrl_target = (PhysicsCharacterController)serializedObject.targetObject;
-
-        // link entre as propriedades do custom editor e o componente
-        skeletonBase = serializedObject.FindProperty("SkeletonBase");   // base do objecto
-
-        // actualiza todos os objectos referenciados
-        serializedObject.Update();
-
-        // chama comportamento de layout
-        Layout();
-
-        // aplica as modificaçoes feitas no editor ao componente
-        serializedObject.ApplyModifiedProperties();
-    }
-
-    // retem todo o layout para o editor
-    private void Layout()
-    {
-        #region Information Region
-        // Info
-        //GUI.backgroundColor = Color.grey; // define a cor de fundo para o bloco
-        // grupo de informação
-        EditorGUILayout.BeginVertical(GUI.skin.label);
-        EditorGUILayout.LabelField("Physics Controller", EditorStyles.boldLabel);
-        EditorGUILayout.HelpBox("Physics Character controller, fill the fields needed", MessageType.Info);
-        //Campo para referencias
-        // Base do esqueleto
-        EditorGUILayout.PropertyField(skeletonBase);
-        EditorGUILayout.EndVertical();
-        #endregion
-
-        EditorGUILayout.Space();
-
-        #region Useful Bones
-        EditorGUILayout.BeginVertical("Box");
-        // Information
-        EditorGUILayout.LabelField("Bone Structure", EditorStyles.boldLabel);
-
-        // avalia se já estao definidos os ossos no componente
-        if (physicsCtrl_target.GetBoneCount == 0)
-        {
-            // caso nao existam elementos
-            EditorGUILayout.HelpBox("Useful Bones not defined!", MessageType.Error);
-            // Botao que calcula determina os ossos
-            if (GUILayout.Button("Validate Bones")) physicsCtrl_target.CalculateBones();
-        }
-        else
-        {
-            // precorre todos os elementos presentes
-            for (int i = 0; i < physicsCtrl_target.GetBoneCount; i++)
-            {
-                // extrai o objecto do elemento a ser trabalhado, e transforma num objecto do tipo "Bone"
-                Bone value = physicsCtrl_target.GetBoneByIndex(i);
-                // Mostra a informação do osso 
-                EditorGUILayout.LabelField(value.Name, EditorStyles.boldLabel);
-                EditorGUILayout.LabelField("Joint Rotation", value.BoneTransform.localRotation.eulerAngles.ToString());
-
-            }
-        }
-
-        EditorGUILayout.Space();
-        EditorGUILayout.EndVertical();
-        #endregion
 
     }
-    #endregion
+
 }
--- Farmers WareHouse - game/Assets/Scripts/RagDoll/PhysicsCharacterController.cs
+++ Farmers WareHouse - game/Assets/Scripts/RagDoll/PhysicsCharacterController.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 public class PhysicsCharacterController : MonoBehaviour
 {
     // Variaveis publicas
-    // Base do modelo
-    public GameObject SkeletonBase;
-    // Lista de todas as joints definidas no modelo
-    [SerializeField] private Bone[] _bones;
+    // referencia para os objectos referenciados como ossos chave
+    public GameObject head;
 
     // Start is called before the first frame update
     void Start()
     {
-        // inicia o comportamento do componente
-        Init();
     }
 
     // Update is called once per frame
@@ -24,47 +20,10 @@ public class PhysicsCharacterController : MonoBehaviour
 
 
     #region Private Methods
-    /// <summary>
-    /// Inicia o componente fisico do controlador do personagem,
-    /// procura por ossos elementais e retem a sua referencia
-    /// </summary>
-    private void Init()
-    {
-        // array de elementos que contenham rigidbodyes e character joints
-        var childs = Array.FindAll(SkeletonBase.GetComponentsInChildren<Rigidbody>(),
-            item => item.GetComponent<CharacterJoint>());
-        // inicia o array de bones
-        _bones = new Bone[childs.Length];
-
-        // debug para o controlador
-        InformationPanel.DebugConsoleInput($"Found {childs.Length} Useful bones");
-
-        // para cada um dos rigidbodys encontrados, cria um bone util e atribui á lista interna
-        for (int i = 0; i < childs.Length; i++)
-        {
-            // avalia o rigidbody e caso seja um osso valido extrai a informaçao para o bone
-            _bones[i] = new Bone(childs[i].gameObject);
-            
-            // adiciona ao controlador de debug
-            InformationPanel.DebugConsoleInput(_bones[i].Name);
-        }
-    }
+    
     #endregion
 
     #region Public Methods
-    /// <summary>
-    /// Retorna o numero de ossos detectados
-    /// </summary>
-    public int GetBoneCount { get { if (_bones != null) return _bones.Length; else return 0; } }
-    /// <summary>
-    /// Retorna o elemento do array de acordo com o indice passado
-    /// </summary>
-    /// <param name="index">Posiçao do osso no array</param>
-    /// <returns></returns>
-    public Bone GetBoneByIndex(int index) { if (GetBoneCount != 0) return _bones[index]; else return null; }
-    /// <summary>
-    /// Inicia o array de ossos
-    /// </summary>
-    public void CalculateBones() => Init();
+
     #endregion
 }
