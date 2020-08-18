a01dedc6ff42a9c5b23b82bbc4012aa13128643c
Eurico Martins <euricomartinss@gmail.com>
2020-08-18 21:00:23 +0100

    active ragdoll moving responding to target animation

---
+++ Farmers WareHouse - game/Assets/Scripts/RagDoll/Editor/PhysicsCharacterControllerEditor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Experimental.TerrainAPI;
+
+// Custom editor for the physics character controller
+[CustomEditor(typeof(PhysicsCharacterController))]
+public class PhysicsCharacterControllerEditor : Editor
+{
+    // serializedProperties
+    SerializedProperty partList;
+
+    //ON enable
+    private void OnEnable()
+    {
+        // set the serialized properties
+        partList = serializedObject.FindProperty("bodyParts");
+    }
+
+    // on inspector GUIO
+    public override void OnInspectorGUI()
+    {
+        // update the serialized object
+        serializedObject.Update();
+        
+        // draw UI
+        DrawUI();
+
+        // Apply changes to serialized obj
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    /// <summary>
+    /// Draw the custum interface
+    /// </summary>
+    private void DrawUI()
+    {
+        // Info label
+        EditorGUILayout.LabelField("Reference all the body parts", EditorStyles.boldLabel);
+        // draw the list of parts
+        EditorGUILayout.PropertyField(partList);
+    }
+}
--- Farmers WareHouse - game/Assets/Scripts/RagDoll/PhysicsCharacterController.cs
+++ Farmers WareHouse - game/Assets/Scripts/RagDoll/PhysicsCharacterController.cs
@@ -1,34 +1,80 @@
 ﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+
+/// <summary>
+/// Struct that organizes the target with the GO and the transform
+/// </summary>
+[System.Serializable]
+public class Part
+{
+    // public information
+    public GameObject partGO;               // reff to the base GO
+    public GameObject partTarget;           // reff to the target GO
+    [HideInInspector] public ConfigurableJoint partJoint;     // reff to the target GO
+    public bool inverting;                  // bool telling if mirroring the target 
+}
 
 public class PhysicsCharacterController : MonoBehaviour
 {
-    // Variaveis publicas
+    #region Private Vars
+    // List of parts
+    [SerializeField] private List<Part> bodyParts = new List<Part>();
+    #endregion
 
-    // private var
-    private GameObject[] parts = new GameObject[11];
-
-    #region Private Methods
     // On Start
     private void Start()
     {
-        // evaluate if all the parts have a GO reffered
-        CheckAllComponents();
-
+        // Init all the partes in list
+        InitParts();
     }
-
-    // Internal methods
-    /// <summary>
-    /// check if all the body components are referend correctly
-    /// </summary>
-    private void CheckAllComponents()
+  
+    // Fixed Update
+    private void FixedUpdate()
     {
+        // update bodyparts target rotation
+        foreach (var part in bodyParts)
+        {
+            // aply the diff to the start joint rotation
+            if (part.inverting)
+                part.partJoint.targetRotation = Quaternion.Inverse(part.partTarget.transform.localRotation);
+            else
+                part.partJoint.targetRotation = part.partTarget.transform.localRotation;
+        }
     }
-    #endregion
 
-    #region Public Methods
-
-
-
-    #endregion
+    /// <summary>
+    /// Collect data from the main objects, get the joint and check if have a valid target
+    /// </summary>
+    private void InitParts()
+    {
+        // evaluate all the elements in list
+        foreach (var bPart in bodyParts)
+        {
+            // evaluate if the main go exist
+            if (bPart.partGO)
+            {
+                // checks if has a configurable joint
+                if (bPart.partGO.GetComponent<ConfigurableJoint>())
+                {
+                    // stores the ref for the joint
+                    bPart.partJoint = bPart.partGO.GetComponent<ConfigurableJoint>();
+                    // evaluate the target GO
+                    if (!bPart.partTarget)
+                    {
+                        // if not setted properly, debug and close
+                        Debug.LogError($"GO -> {bPart.partGO.name} doesnt have a valid target!");
+                        EditorApplication.ExitPlaymode();
+                    }
+                }
+                else
+                {
+                    //if fails and its in editor, stop and debug
+                    Debug.LogError($"GO -> {bPart.partGO.name} doesnt have a Configurable joint!");
+                    EditorApplication.ExitPlaymode();
+                }
+            }
+        }
+    }
 }
